b7557a149936025d37d39fce8ba6afc6d3395dd7
caizl.hunter <caizl@hunter.dev>
2019-12-28 11:47:18 +0800

    zipkin

--- src/main/java/com/mycloud/conf/repo/Application.java
+++ src/main/java/com/mycloud/conf/repo/Application.java
@@ -1,7 +1,9 @@
 package com.mycloud.conf.repo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableZipkinServer
 public class Application {
 }
